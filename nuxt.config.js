3a7b32d1d6d60ad47efeba9d72c0ab3e5be7761a
wataru.chame.gon@gmail.com <wataru.chame.gon@gmail.com>
2020-03-04 19:12:56 +0900

    head settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,17 +4,16 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: '学生団体UKARI',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: '札幌の学生団体UKARIです。'
       }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    ]
   },
   /*
    ** Customize the progress-bar color
