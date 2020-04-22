3d5d229063400cb938fcd2a9465a2389d77e1c68
WataruNishimura <wataru.nishimura@nnkk.work>
2020-04-22 15:14:45 +0900

    add loading settings to iframe in top page

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     section.page__section
       h1.page__title
         img(src="/img/logos/UKARI_LOGO.svg" alt="UKRAI Logo" decoding="async" loading="lazy")
+        
     section.page__section
       h2.page__subtitle About UKARI
       p.page__text UKARIは「挑戦者のベースキャンプ」を理念とし、札幌を始めとして多くの高校生によって作りだされるコミュニティです。
@@ -23,7 +24,7 @@
           <a class="twitter-timeline" data-lang="ja" data-width="300" data-height="400" data-theme="dark" href="https://twitter.com/UKARI2018?ref_src=twsrc%5Etfw">Tweets by UKARI2018</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         .sns-group__box
           h3.page__small-title Facebook
-          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FUKARI2018%2F&tabs=timeline&width=300&height=400&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="300" height="400" style="overflow:hidden;border:none" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FUKARI2018%2F&tabs=timeline&width=300&height=400&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId" width="300" height="400" style="overflow:hidden;border:none" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" loading="lazy"></iframe>
         .sns-group__box 
           h3.page__small-title note
           noteViewer(:notes="jsonData")
